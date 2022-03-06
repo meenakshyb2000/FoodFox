57a8efa7b84f6629f0dcfff95ed023d751e5578a
Meenakshy Balakrishnan <74719217+meenakshyb2000@users.noreply.github.com>
2022-03-06 11:07:40 +0530

    Update Main.java

--- FoodFox/test/src/application/Main.java
+++ FoodFox/test/src/application/Main.java
@@ -5,25 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
+//	public void setId()
+//        {
+//             
+//        }
+  
+	public static void main(String[] args) {
+            
+		launch(args);
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage arg0) throws Exception {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("HomePageScene.fxml"));
-			Scene scene = new Scene(root);
+			Parent root=FXMLLoader.load(getClass().getResource("/application/login.fxml"));
+                        Scene scene=new Scene(root,1300,700);
 			
-			primaryStage.setTitle("Home Page");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			arg0.setTitle("FoodFox"); //arg0 is the primary stage
+			arg0.setScene(scene);
+			arg0.setResizable(false);
+			arg0.show();
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
+		
 	}
 }
