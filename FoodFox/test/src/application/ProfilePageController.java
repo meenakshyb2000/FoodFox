6899baa1fa71f4f3a88ce2c25db8ad43b189bd1a
pavithrashaji <70861378+pavithrashaji@users.noreply.github.com>
2022-03-06 21:35:22 +0530

    Update ProfilePageController.java

--- FoodFox/test/src/application/ProfilePageController.java
+++ FoodFox/test/src/application/ProfilePageController.java
@@ -1,12 +1,15 @@
 package application;
 
 import java.io.IOException;
+import java.sql.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class ProfilePageController {
@@ -25,7 +28,30 @@ public class ProfilePageController {
 
     @FXML
     private Button logout;
+    
+    @FXML
+    private Label fnamelabel;
+    
+    @FXML
+    private Label lnamelabel, unamelabel, conlabel, maillabel, loclabel, ptslabel;
+    
+    @FXML
+    private Label rlabel, falabel, eptslabel, dloclabel;
+    
+    @FXML
+    private Label fl1, fl2, fl3, fl4, fl5, fl6, fl7, fl8, fl9, fl10;
+    
+    @FXML
+    private Label q1, q2, q3, q4, q5, q6, q7, q8, q9, q10;
+    
+    @FXML
+    private Label a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
 
+    Connection con1;
+    PreparedStatement ps1;
+    ResultSet rs1;
+    
+    
     @FXML
     void accountbtn(ActionEvent event) {
         try
@@ -87,6 +113,152 @@ public class ProfilePageController {
         {
 			e.printStackTrace();
 	}	
+    }
+    
+    public void initialize()
+    {
+    	int c=1;
+    	try
+    	{
+    		 Class.forName("com.mysql.cj.jdbc.Driver");
+    		 con1=DriverManager.getConnection("jdbc:mysql://localhost:3306/foodfoxdatabase","root","chimera");
+    		 ps1=con1.prepareStatement("select * from Customer where CID='C2'");
+    		 rs1=ps1.executeQuery();
+    		 while(rs1.next())
+    		 {
+    			 fnamelabel.setText(rs1.getString(2));
+    			 lnamelabel.setText(rs1.getString(3));
+    			 unamelabel.setText(rs1.getString(8));
+    			 conlabel.setText(rs1.getString(4));
+    			 maillabel.setText(rs1.getString(5));
+    			 loclabel.setText(rs1.getString(6));
+    			 ptslabel.setText(rs1.getString(7));	 
+    		 }
+    		 
+    		 ps1=con1.prepareStatement("select * from OrderDetails where OrderID='O1'");
+    		 rs1=ps1.executeQuery();
+    		 while(rs1.next())
+    		 {
+    			 rlabel.setText(rs1.getString(3));
+    			 falabel.setText(rs1.getString(4));
+    			 eptslabel.setText(rs1.getString(5));
+    			 dloclabel.setText(rs1.getString(6));	 
+    		 }
+    		 
+    		 for(int i=1; i<=10; i++)
+    		 {
+    			 String query = "select * from Order1 where FoodID = 'O1F" + i+"'";
+    			 ps1=con1.prepareStatement(query);
+    			 rs1=ps1.executeQuery();
+    			 
+    			 if(i==1)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl1.setText(rs1.getString(4));
+        				 q1.setText(rs1.getString(5));
+        				 a1.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==2)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl2.setText(rs1.getString(4));
+        				 q2.setText(rs1.getString(5));
+        				 a2.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==3)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl3.setText(rs1.getString(4));
+        				 q3.setText(rs1.getString(5));
+        				 a3.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==4)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl4.setText(rs1.getString(4));
+        				 q4.setText(rs1.getString(5));
+        				 a4.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==5)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl5.setText(rs1.getString(4));
+        				 q5.setText(rs1.getString(5));
+        				 a5.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==6)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl6.setText(rs1.getString(4));
+        				 q6.setText(rs1.getString(5));
+        				 a6.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==7)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl7.setText(rs1.getString(4));
+        				 q7.setText(rs1.getString(5));
+        				 a7.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==8)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl8.setText(rs1.getString(4));
+        				 q8.setText(rs1.getString(5));
+        				 a8.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==9)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl9.setText(rs1.getString(4));
+        				 q9.setText(rs1.getString(5));
+        				 a9.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    			 else if(i==10)
+    			 {
+    				 while(rs1.next())
+        			 {
+        				 fl10.setText(rs1.getString(4));
+        				 q10.setText(rs1.getString(5));
+        				 a10.setText(rs1.getString(6));
+        			 } 
+    			 }
+    			 
+    		 }
+    		 
+    	}
+    	catch(ClassNotFoundException | SQLException e)
+        {
+    		System.out.println("Connection failed");
+    		System.out.println(e);
+        }
     }
 
 }
